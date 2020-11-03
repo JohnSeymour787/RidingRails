8980ab1304a80e8c1cc4e8c140a6aca438fbe507
John Seymour <john.seymour787@gmail.com>
2020-11-04 10:31:34 +1100

    Updated StopDetailsDeserialiser to handle deserialision of StopDetails from either the API response or reading from local storage.

--- app/src/main/java/com/johnseymour/ridingrails/apisupport/StopDetailsDeserialiser.kt
+++ app/src/main/java/com/johnseymour/ridingrails/apisupport/StopDetailsDeserialiser.kt
@@ -1,8 +1,6 @@
 package com.johnseymour.ridingrails.apisupport
 
-import com.google.gson.JsonDeserializationContext
-import com.google.gson.JsonDeserializer
-import com.google.gson.JsonElement
+import com.google.gson.*
 import com.johnseymour.ridingrails.models.data.StopDetails
 import com.johnseymour.ridingrails.models.data.TravelMode
 import java.lang.NullPointerException
@@ -13,48 +11,58 @@ internal object StopDetailsDeserialiser: JsonDeserializer<StopDetails>
 {
     override fun deserialize(json: JsonElement?, typeOfT: Type?, context: JsonDeserializationContext?): StopDetails?
     {
-        val locationsArray = json?.asJsonObject?.getAsJsonArray("locations")
+        val topLevelJSON = json?.asJsonObject ?: return null
 
-        //Searches the location's array in the response and returns the first appropriate location
-        // (ie, a location that serves trains in this case, should mostly be the first element of this
-        // array anyway)
-        locationsArray?.forEach { element ->
-
-            element?.asJsonObject?.apply {
-
-                val id = getAsJsonPrimitive("id").asInt
-                val name = getAsJsonPrimitive("name").asString ?: ""
-                val disassembledName = getAsJsonPrimitive("disassembledName").asString?: ""
-
-                //Attempt to get a valid list of modes, where TravelMode.Train is one of them
-                try
-                {
-                    //Not all location responses have a mode array
-                    val modeIntsArray = getAsJsonArray("modes").asJsonArray?.let {
-                        //Get the integer list of modes
-                        context?.deserialize<Array<Int>>(it, Array<Int>::class.java)?.toList()
-                    }?: return@forEach      //Technically unreachable because asJsonArray will throw an error first
-
-                    //If there are no trains available at this stop, also skip it
-                    if (!trainConnection(modeIntsArray)) {return@forEach}
-
-                    //Converting mode integer list into TravelMode list
-                    val modes = mutableListOf<TravelMode>()
-                    modeIntsArray.forEach { modeInt ->
-                        TravelMode.valueOf(modeInt)?.let {
-                            modes.add(it)
-                        }
-                    }
-                    return StopDetails(id, name, disassembledName, modes)
-                }
-                catch (e: NullPointerException)
-                {
-                    //Continue in the loop if no modes array
-                    return@forEach
-                }
+        topLevelJSON.getAsJsonArray("locations")?.forEach { element ->
+            //Searches the location's array in the response and returns the first appropriate location
+            // (ie, a location that serves trains in this case, should mostly be the first element of this
+            // array anyway)
+            element?.asJsonObject?.let {
+                return deserialiseSingleStop(it, context) ?: return@forEach
             }
         }
-        return null
+        //Otherwise, assume this JSON is for a single StopDetails object, so try to deserialise it like one
+        return deserialiseSingleStop(topLevelJSON, context)
+    }
+
+    private fun deserialiseSingleStop(stopTopLevel: JsonObject, context: JsonDeserializationContext?): StopDetails?
+    {
+        with (stopTopLevel)
+        {
+            val id = getAsJsonPrimitive("id").asInt
+            val name = getAsJsonPrimitive("name").asString ?: ""
+            val disassembledName = getAsJsonPrimitive("disassembledName").asString ?: ""
+
+            //Try to convert the travel modes directly from the enum values (if deserialising from the text file)
+            getAsJsonArray("travelModes")?.let {travelModesArray ->
+                context?.deserialize<Array<TravelMode>>(travelModesArray, Array<TravelMode>::class.java)?.toList()?.let {
+                    return StopDetails(id, name, disassembledName, it)
+                }
+            }
+            //Otherwise. attempt to get a valid list of modes from an array of ints returned from the API, where TravelMode.Train is one of them
+            val modes = convertToModes(getAsJsonArray("modes"), context) ?: return null
+
+            return StopDetails(id, name, disassembledName, modes)
+        }
+    }
+
+    private fun convertToModes(modeIntsJSON: JsonArray?, context: JsonDeserializationContext?): List<TravelMode>?
+    {
+        //Get the integer list of modes
+        val modeIntsArray = context?.deserialize<Array<Int>>(modeIntsJSON, Array<Int>::class.java)?.toList() ?: return null
+
+        //If there are no trains available at this stop, also skip it
+        if (!trainConnection(modeIntsArray)) { return null }
+
+        //Converting mode integer list into TravelMode list
+        val modes = mutableListOf<TravelMode>()
+        modeIntsArray.forEach { modeInt ->
+            TravelMode.valueOf(modeInt)?.let {
+                modes.add(it)
+            }
+        }
+
+        return modes
     }
 
     private fun trainConnection(modeInts: List<Int>): Boolean
