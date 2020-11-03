89f17864968bcc288f1581765ba122cbdae53129
John Seymour <john.seymour787@gmail.com>
2020-11-03 14:02:38 +1100

    Updated TripOptions ViewModel to remember when a network request was made, and if an error has already occurred, to avoid lifecycle issues.

--- app/src/main/java/com/johnseymour/ridingrails/models/TripOptionsViewModel.kt
+++ app/src/main/java/com/johnseymour/ridingrails/models/TripOptionsViewModel.kt
@@ -9,16 +9,26 @@ import com.johnseymour.ridingrails.models.data.TripJourney
 
 class TripOptionsViewModel: ViewModel()
 {
+    /*As there are multiple LiveData's to observe, and any one of which can fail,
+    * only want to make sure that one error message is shown to the user.
+    **/
+    var networkErrorOccurred = false
+
     lateinit var initialStopLive: LiveData<StatusData<StopDetails>>
     lateinit var finalDestinationLive: LiveData<StatusData<StopDetails>>
     lateinit var journeyOptionsLive: LiveData<StatusData<List<TripJourney>>>
 
+    //Used to prevent multiple network calls for same data when activity has configuration change
+    private var requestMade = false
+
     fun startTripPlan(origin: String, destination: String, dateString: String, timeString: String)
     {
+        if (requestMade) {return}
         NetworkRepository.planTrip(origin, destination, dateString, timeString).let {
             initialStopLive = it.initialStopLive
             finalDestinationLive = it.finalDestinationLive
             journeyOptionsLive = it.journeyOptionsLive
         }
+        requestMade = true
     }
 }
