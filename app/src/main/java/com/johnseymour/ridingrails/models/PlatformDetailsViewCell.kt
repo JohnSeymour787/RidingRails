cc18528281b53d165d010bd2f73542b12b13518f
John <john.seymour787@gmail.com>
2021-01-08 15:13:36 +1100

    Updated PlatformDetailsViewCell to include a custom attribute for the type of platform that it will be representing.

--- app/src/main/java/com/johnseymour/ridingrails/models/PlatformDetailsViewCell.kt
+++ app/src/main/java/com/johnseymour/ridingrails/models/PlatformDetailsViewCell.kt
@@ -5,8 +5,36 @@ import android.util.AttributeSet
 import android.view.LayoutInflater
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.johnseymour.ridingrails.R
+import kotlinx.android.synthetic.main.list_cell_platform_details.view.*
 
 class PlatformDetailsViewCell(context: Context, attrs: AttributeSet? = null): ConstraintLayout(context, attrs)
 {
-    init {(context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as? LayoutInflater)?.inflate(R.layout.list_cell_platform_details, this, true)}
+    init
+    {
+        (context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as? LayoutInflater)?.inflate(R.layout.list_cell_platform_details, this, true)
+
+        //Setting the time1 and time2 label texts depending on this custom attribute
+        // see comments in the PlatformDetails class for explanations
+        when (attrs?.getAttributeIntValue(R.styleable.PlatformDetailsVieCell_PlatformDetailsType, 0))
+        {
+            //Origin times
+            0 ->
+            {
+                time1Label.text = resources.getText(R.string.list_cell_platform_details_time_label_est_dep)
+                time2Label.text = resources.getText(R.string.list_cell_platform_details_time_label_pln_dep)
+            }
+            //Destination times
+            2 ->
+            {
+                time1Label.text = resources.getText(R.string.list_cell_platform_details_time_label_pln_arr)
+                time2Label.text = resources.getText(R.string.list_cell_platform_details_time_label_est_arr)
+            }
+            //StopSequence times
+            else ->
+            {
+                time1Label.text = resources.getText(R.string.list_cell_platform_details_time_label_pln_arr)
+                time2Label.text = resources.getText(R.string.list_cell_platform_details_time_label_pln_dep)
+            }
+        }
+    }
 }
